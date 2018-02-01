05af6cfebfdaae9ee017df63c6530f2a502e26a7
DastonZerg <dastonzerg93@gmail.com>
2018-02-01 12:54:22 +0800

    heap_finished

--- MinHeap.java
+++ MinHeap.java
@@ -28,9 +28,93 @@ public class MinHeap
 	 * 
 	 * @param val the value to be added to the heap
 	 */
+	
+	private void sink()
+	{
+	  int k=1;
+	  int leftChild=k*2;
+	  int rightChild=k*2+1;
+	  CompareInt temp;
+	  while(leftChild<=size)
+	  {
+	    if(leftChild==size)
+	    {
+	      if(heap[k].compareTo(heap[leftChild])>0)
+	      {
+  	      temp=heap[k];
+  	      heap[k]=heap[leftChild];
+  	      heap[leftChild]=temp;
+	      }
+	      else
+	      {
+	        break;
+	      }
+	    }
+	    else
+	    {
+	      if(heap[leftChild].compareTo(heap[rightChild])<0)
+	      {
+	        if(heap[k].compareTo(heap[leftChild])<0)
+	        {
+	          break;
+	        }
+	        else
+	        {
+  	        temp=heap[k];
+  	        heap[k]=heap[leftChild];
+  	        heap[leftChild]=temp;
+  	        k=leftChild;
+  	        leftChild=k*2;
+  	        rightChild=k*2+1;
+	        }
+	      }
+	      else
+	      {
+	        if(heap[k].compareTo(heap[rightChild])<0)
+	        {
+	          break;
+	        }
+	        else
+	        {
+  	        temp=heap[k];
+  	        heap[k]=heap[rightChild];
+  	        heap[rightChild]=temp;
+  	        k=rightChild;
+            leftChild=k*2;
+            rightChild=k*2+1;
+	        }
+	      }
+	    }
+	  }
+	}
+	
+	private void swin()
+	{
+	  int k=size;
+	  int nextK=k/2;
+	  while(k>1 && heap[nextK].compareTo(heap[k])>0)
+	  {
+	    CompareInt temp;
+	    temp=heap[nextK];
+	    heap[nextK]=heap[k];
+	    heap[k]=temp;
+	    k=nextK;
+	    nextK=k/2;
+	  }
+	}
+	
 	public void add(CompareInt val) 
 	{
-		return;
+		if(size==heap.length-1)
+		{
+		  return;
+		}
+		else
+		{
+		  size++;
+		  heap[size]=val;
+		  swin();
+		}
 	}
 	
 	/**
@@ -38,8 +122,18 @@ public class MinHeap
 	 */
 	public CompareInt extractMin() 
 	{
-		return null;
+	  if(size<1)
+	  {
+	    return null;
+	  }
+	  else
+	  {
+  	  CompareInt min=heap[1];
+  		heap[1]=heap[size];
+  		size--;
+  		sink();
+  		return min;
+	  }
 	}
-	
 	
 }
